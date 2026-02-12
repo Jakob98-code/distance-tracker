6f53e7866954fa42c85563b3bdd901f710240185
Jakob98-code <Jakob98-code@users.noreply.github.com>
2026-02-12 18:56:56 +0000

    deploy: 9bc768d01a777b9f42127ffbef7dc7f9165df134

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
-window.FIREBASE_CONFIG = {"apiKey":"AIzaSyAb94k7A825Lsc8HJYFeTvl_IrS9GAoPpQ","authDomain":"distance-tracker-c8bbf.firebaseapp.com","databaseURL":"https://distance-tracker-c8bbf-default-rtdb.europe-west1.firebasedatabase.app","projectId":"distance-tracker-c8bbf","storageBucket":"distance-tracker-c8bbf.firebasestorage.app","messagingSenderId":"193644544453","appId":"1:193644544453:web:4e02be877cf2267e6ccbac","measurementId":"G-4Q1V14YHSS"};
+window.FIREBASE_CONFIG = {
+    "apiKey": "AIzaSyAb94k7A825Lsc8HJYFeTvl_IrS9GAoPpQ",
+    "authDomain": "distance-tracker-c8bbf.firebaseapp.com",
+    "databaseURL": "https://distance-tracker-c8bbf-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "distance-tracker-c8bbf",
+    "storageBucket": "distance-tracker-c8bbf.firebasestorage.app",
+    "messagingSenderId": "193644544453",
+    "appId": "1:193644544453:web:4e02be877cf2267e6ccbac",
+    "measurementId": "G-4Q1V14YHSS"
+};
